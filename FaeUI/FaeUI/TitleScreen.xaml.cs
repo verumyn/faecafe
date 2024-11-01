931faa0438bbf9dbb0779b5d93bbb1c6811f3423
verumyn <leighlynch2020@gmail.com>
2024-11-01 19:36:48 -0400

    Added buttons to title screen

---
+++ FaeUI/FaeUI/TitleScreen.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FaeUI
+{
+    /// <summary>
+    /// Interaction logic for TitleScreen.xaml
+    /// </summary>
+    public partial class TitleScreen : Window
+    {
+        public TitleScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void StartNewGame(object sender, RoutedEventArgs e)
+        {
+            //GameScreen gameScreen = new();
+        }
+
+        private void ContinueLastSave(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void QuitGame(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowLoadingMenu(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowOptions(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowCredits(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
